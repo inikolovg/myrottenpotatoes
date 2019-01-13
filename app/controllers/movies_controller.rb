9ac1f8c363486b7bbf22e7b16c8fbfd107d3e1b0
Ilian Georgiev <inikolovg@gmail.com>
2019-01-13 20:28:16 +0000

    Ex 7-1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,10 +13,33 @@ class MoviesController < ApplicationController
   # default: render 'new' template
  end
  def create
- params.require(:movie)
- permitted = params[:movie].permit(:title,:rating,:release_date)
- @movie = Movie.create!(permitted)
- redirect_to movies_path
+  params.require(:movie)
+  permitted = params[:movie].permit(:title,:rating,:release_date)
+  @movie = Movie.new(permitted)
+  if @movie.save
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  else
+    render 'new' # note, 'new' template can access @movie's field values!
+  end
+ end
+ def edit
+  @movie = Movie.find params[:id]
+ end
+ 
+ def update
+  @movie = Movie.find params[:id]
+  params.require(:movie)
+  permitted = params[:movie].permit(:title,:rating,:release_date)
+  @movie.update_attributes!(permitted)
+  flash[:notice] = "#{@movie.title} was successfully updated."
+  redirect_to movie_path(@movie)
+ end
+ def destroy
+  @movie = Movie.find(params[:id])
+  @movie.destroy
+  flash[:notice] = "Movie '#{@movie.title}' deleted."
+  redirect_to movies_path
  end
 end
 
